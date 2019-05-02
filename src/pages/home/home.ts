4a7ce98cff5532a3a136e5c7b7b83928195fe424
unknown <sureshkumar20141@gmail.com>
2019-05-02 18:04:06 +0530

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ImagePicker } from '@ionic-native/image-picker';
+import * as EXIF from 'exif-js';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,56 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  img:string;
+  isPhoto :boolean= false;
+  constructor(
+      public navCtrl: NavController,
+      private imagePicker: ImagePicker
+    ) {
+    this.img ="../../assets/imgs/noImg.jpg";
   }
 
+
+  loaded(e) {
+    alert('load');
+    alert(JSON.stringify(e));
+    console.log(e);
+    EXIF.getData(e.target, function () {
+      //console.log(e.target);
+      var allMetaData = EXIF.getAllTags(this);
+      console.log(allMetaData);
+      var make = EXIF.getTag(this, "Make");
+      var model = EXIF.getTag(this, "Model");
+      console.log(make);
+      console.log(model);
+      console.log(EXIF.getTag(this, "DateTime"));
+
+      alert(make);
+      alert(model);
+      alert(EXIF.getTag(this, "DateTime"));
+      alert(JSON.stringify(allMetaData));
+    });
+  }
+
+  openGallery() {
+    let options = {
+      title :'Select Picture',
+      message :'Select Atleast one picture',
+      maximumImagesCount:1,
+      outputType:0
+
+    };
+    this.imagePicker.getPictures(options).then((results) => {
+      for (var i = 0; i < results.length; i++) {
+        this.img = results[i];        
+        console.log('Image URI: ' + results[i]);
+        alert('Image URI: ' + results[i]);
+
+        //this.loaded(results[i]);
+        this.isPhoto = true;
+      }
+    }, (err) => { 
+      alert(JSON.stringify(err));
+    });
+  }
 }
